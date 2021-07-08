47bd9a122878a7a148c5330fe170325ea50d136f
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-08 19:51:17 +0300

    Add boilerplate for webpack 5

---
+++ boilerplate/src/index.js
@@ -0,0 +1,8 @@
+// import helloWorld form "./hello-world.js" //for this will need to specify a loader
+// const helloWorld = require("./hello-world");
+import HelloWorldButton from "./components/hello-world-button/hello-world-bitton";
+import addImage from "./add-image"
+
+const helloWorldButton = new HelloWorldButton()
+helloWorldButton.render()
+// addImage();
