6ff52ac16656ae5ff5181b2c7942e1c9ca741052
Taibu Haroub <haroubtaibu@gmail.com>
2021-07-08 22:13:27 +0300

    Extract css into a separate file

--- working/src/index.js
+++ working/src/index.js
@@ -2,6 +2,10 @@
 // const helloWorld = require("./hello-world");
 import HelloWorldButton from "./components/hello-world-button/hello-world-bitton";
 import addImage from "./add-image"
+import Heading from "./components/heading/heading"
+
+const heading = new Heading();
+heading.render();
 
 const helloWorldButton = new HelloWorldButton()
 helloWorldButton.render()
